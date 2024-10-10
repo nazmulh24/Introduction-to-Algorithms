98926a8c76f0e966e7aa3569de08767f85ca83e8
Nazmul Hossain <snazmulhossains24@gmail.com>
2024-10-11 00:56:02 +0600

    Code of Unbounded Knapsack (top down + bottom up)

--- Introduction to Algorithms/Week_6_Bonus/24_10_09_module_20/prog_1.cpp
+++ Introduction to Algorithms/Week_6_Bonus/24_10_09_module_20/prog_1.cpp
@@ -1,6 +1,6 @@
 /***********************************************************************
 
-Name --> Unbounded Knapsack Implementation
+Name --> Unbounded Knapsack Implementation --> Top-Down
 Link -->
 
 ***********************************************************************/
@@ -23,6 +23,31 @@ using namespace std;
 #define UNIQUE(v) v.erase(unique(all(v)), v.end())
 //********************************************************************
 
+int dp[1001][1001];
+
+int unbounded_knapsack(int n, int val[], int wei[], int w)
+{
+    if (n == 0 || w == 0)
+        return 0;
+
+    if (dp[n][w] != -1)
+        return dp[n][w];
+
+    if (wei[n - 1] <= w)
+    {
+        int op1 = val[n - 1] + unbounded_knapsack(n, val, wei, w - wei[n - 1]);
+        int op2 = unbounded_knapsack(n - 1, val, wei, w);
+
+        return dp[n][w] = max(op1, op2);
+    }
+    else
+    {
+        int op2 = unbounded_knapsack(n - 1, val, wei, w);
+
+        return dp[n][w] = op2;
+    }
+}
+
 void solve()
 {
     int n, w;
@@ -37,7 +62,11 @@ void solve()
     for (int i = 0; i < n; i++)
         cin >> weight[i];
 
-    
+    memset(dp, -1, sizeof(dp));
+
+    int ans = unbounded_knapsack(n, value, weight, w);
+
+    cout << ans << endl;
 }
 
 int32_t main()
