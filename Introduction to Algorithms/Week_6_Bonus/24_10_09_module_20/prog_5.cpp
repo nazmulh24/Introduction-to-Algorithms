4fedb4b0a04a643c11831bc18fefe8d2de94bbee
Nazmul Hossain <snazmulhossains24@gmail.com>
2024-10-11 15:34:31 +0600

    Unbounded Subset_sum (Coin Change-1) Problem Code...

---
+++ Introduction to Algorithms/Week_6_Bonus/24_10_09_module_20/prog_5.cpp
@@ -0,0 +1,83 @@
+/***********************************************************************
+
+Name --> Coin Change 1
+Link --> Unbounded Subset_sum( Kotovabe Sum banano jay... )
+
+***********************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define per(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define trav(a, x) for (auto &a : x)
+#define UNIQUE(v) v.erase(unique(all(v)), v.end())
+//********************************************************************
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int sum;
+    cin >> sum;
+
+    int dp[n + 1][sum + 1];
+
+    dp[0][0] = 1;
+    for (int i = 1; i <= sum; i++)
+        dp[0][i] = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j <= sum; j++)
+        {
+            if (a[i - 1] <= j)
+            {
+                int op1 = dp[i][j - a[i - 1]]; //--> Knapsack_subset er eijaygay change...
+                int op2 = dp[i - 1][j];
+
+                dp[i][j] = (op1 + op2);
+            }
+            else
+            {
+                int op2 = dp[i - 1][j];
+
+                dp[i][j] = op2;
+            }
+        }
+    }
+
+    int ans = dp[n][sum];
+
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    // cin >> t; // Uncomment if multiple test cases
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
