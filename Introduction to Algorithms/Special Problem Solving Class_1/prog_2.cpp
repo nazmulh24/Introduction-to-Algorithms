884b89a3f8e9f8786189a17ee73f9c461fe90296
Nazmul Hossain <snazmulhossains24@gmail.com>
2024-10-12 22:36:13 +0600

    Palindrome Reorder

--- Introduction to Algorithms/Special Problem Solving Class_1/prog_2.cpp
+++ Introduction to Algorithms/Special Problem Solving Class_1/prog_2.cpp
@@ -28,7 +28,53 @@ void solve()
     string s;
     cin >> s;
 
-    
+    map<char, int> cnt;
+
+    int n = s.size();
+    for (int i = 0; i < n; i++)
+        cnt[s[i]]++; //--> Frequency Array...
+
+    int odd_cnt = 0;
+    for (auto val : cnt)
+    {
+        if (val.se % 2 != 0)
+            odd_cnt++;
+    }
+
+    if (odd_cnt > 1)
+    {
+        cout << "NO SOLUTION" << endl;
+        return;
+    }
+
+    string first, mid, last;
+
+    for (auto val : cnt)
+    {
+        int key = val.fi;
+        int occ = val.se;
+
+        if (occ % 2 != 0)
+        {
+            for (int i = 1; i <= occ; i++)
+            {
+                mid.pb(key);
+            }
+        }
+        else
+        {
+            for (int i = 1; i <= (occ / 2); i++)
+            {
+                first.pb(key);
+                last.pb(key);
+            }
+        }
+    }
+
+    reverse(last.begin(), last.end());
+    string ans = first + mid + last;
+
+    cout << ans << endl;
 }
 
 int32_t main()
