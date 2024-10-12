f4d822edec5bd31942aa1a5d56734fccbbbab9a9
Nazmul Hossain <snazmulhossains24@gmail.com>
2024-10-12 21:50:49 +0600

    codeforce-> B. Sereja and Suffixes

---
+++ Introduction to Algorithms/Special Problem Solving Class_1/prog_1.cpp
@@ -0,0 +1,67 @@
+/***********************************************************************
+
+Name --> B. Sereja and Suffixes
+Link --> https://codeforces.com/contest/368/problem/B
+
+***********************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define per(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define trav(a, x) for (auto &a : x)
+#define UNIQUE(v) v.erase(unique(all(v)), v.end())
+//********************************************************************
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    vector<int> cnt(n + 1);
+
+    set<int> s;
+    for (int i = n; i > 0; i--)
+    {
+        s.insert(a[i]);
+        cnt[i] = s.size();
+    }
+
+    while (m--)
+    {
+        int l;
+        cin >> l;
+
+        cout << cnt[l] << endl;
+    }
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    // cin >> t; // Uncomment if multiple test cases
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
