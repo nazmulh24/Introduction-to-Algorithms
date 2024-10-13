a4fe11652abaa5b6688eae3140bdc8cea2464aae
Nazmul Hossain <snazmulhossains24@gmail.com>
2024-10-13 22:11:51 +0600

    B. Robin Hood and the Major Oak

---
+++ Introduction to Algorithms/Special Problem Solving Class_2/prog_2.cpp
@@ -0,0 +1,55 @@
+/***********************************************************************
+
+Name --> B. Robin Hood and the Major Oak
+Link --> https://codeforces.com/contest/2014/problem/B
+
+***********************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define per(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define trav(a, x) for (auto &a : x)
+#define UNIQUE(v) v.erase(unique(all(v)), v.end())
+//********************************************************************
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    int ans = 0;
+
+    if (n % 2 == 0)
+        ans = k / 2;
+    else
+        ans = ceil(k / 2.0);
+
+    cout << ((ans % 2 == 0) ? "YES" : "NO") << endl;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    cin >> t; // Uncomment if multiple test cases
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
